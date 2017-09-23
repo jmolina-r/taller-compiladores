acda65bd8a48b0120335f7dc75db3d1e1478e990
jmolina-r <jmolina.ok@gmail.com>
2017-09-23 13:39:12 -0300

    - se definen las reglas para Números, identificadores,comentarios, caracteres ingnoradso y manejo de erroes.

--- scanner.py
+++ scanner.py
@@ -25,21 +25,33 @@ tokens = (
     'LBRACE',
     'RBRACE',
     'LSQUAREBRACKET',
-    'RSQUAREBRACKET'
+    'RSQUAREBRACKET',
     'LESS',
     'GREATER',
     'MUCHSMALLER',
     'MUCHGREATER',
     'EQUAL',
-    'INEGUAL'
+    'INEGUAL',
     'COMMA',
     'SEMICOLON',
-    'ASSIGN'
+    'ASSIGN',
     'MULTCOMMMENT',
     'LINECOMMENT',
     'ID',
     'NUM'
 )
+
+#listado de palabras reservadas (key: valor )
+reserved = {
+    'else': 'ELSE',
+    'if': 'IF',
+    'int': 'INT',
+    'void': 'VOID',
+    'return': 'RETURN',
+    'while': 'WHILE'
+}
+
+tokens = tokens+tuple(reserved.values())
 
 # Reglas de las expresiones regulares para tokens simples
 t_PLUS    = r'\+'
@@ -52,7 +64,7 @@ t_LBRACE = r'\{'
 t_RBRACE = r'\}'
 t_LSQUAREBRACKET = r'\['
 t_RSQUAREBRACKET = r'\]'
-t_LESS = r'\<'
+t_LESS = r'<'
 t_GREATER = r'\>'
 t_MUCHSMALLER = r'\<<'
 t_MUCHGREATER = r'\>>'
@@ -60,10 +72,42 @@ t_EQUAL = r'\=='
 t_INEGUAL = r'\<>'
 t_COMMA = r'\,'
 t_SEMICOLON = r'\;'
-t_ASSIGN = r'\='
+t_ASSIGN = r'='
+
+t_MULTCOMMMENT = r'<\/(\s*|.*?)*\/>'
+t_LINECOMMENT = r'((\?|!).*)'
+
+# Regla que contiene caracteres ignorados (espacios y tabs)
+t_ignore  = ' \t'
 
 
-reserved = {
+def t_ID (t):
+    r'[A-Za-z]((_?[a-zA-Z]+)*_?([a-z0-9])+)?'
+    # El método get () devuelve un valor para la key dada. Si la key no está disponible en el diccionario, entonces devuelve el valor predeterminado 'ID'.
+    t.type = reserved.get(t.value.lower(), 'ID')
+    return t
 
 
-}
+# Regla de manejo de errores
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def t_NUM(t):
+    r'(0[0-9])|[1-9][0-9]+'
+    t.value = int(t.value)
+    return t
+
+# Construir el lexer
+lexer = lex.lex()
+
+# Dar al analizador léxico algún input
+lexer.input('<a_=a''8')
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # no hay mas input
+    print(tok)
+

ba140f87645c139c39134fd0b0a0bc084bb1f9eb
jmolina-r <jmolina.ok@gmail.com>
2017-09-23 21:36:17 -0300

    -se implementa la lectura de archivos para realizar el análisis léxico.

--- scanner.py
+++ scanner.py
@@ -1,11 +1,11 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # scanner.py
 #
 # Nombre: Jorge Molina Rojas
-# Ingenieria Civil en Computación e Informática
+# Ingeniería Civil en Computación e Informática
 # Universidad Católica del Norte, Antofagasta, Chile.
 #
-# El trabajo del analizador lexico o scanner, es transdormar un flujo de caracteres
+# El trabajo del analizador léxico o scanner, es transformar un flujo de caracteres
 # en un flojo de tokers. Los tokes a evaluar son los siguientes:
 # -key words: ELSE IF INT VOID RETURN WHILE
 # -simbolos especiales: + - * / < <= > >= == <> = ( ) [ ] { } ! ? </ /> , ;
@@ -13,6 +13,10 @@
 # -números (NUM)
 # -----------------------------------------------------------------------------
 import ply.lex as lex
+import os
+import codecs
+from os import scandir, getcwd
+import sys
 
 # lista de los nombres de los tokens
 tokens = (
@@ -41,7 +45,7 @@ tokens = (
     'NUM'
 )
 
-#listado de palabras reservadas (key: valor )
+# listado de palabras reservadas (key: valor )
 reserved = {
     'else': 'ELSE',
     'if': 'IF',
@@ -51,15 +55,15 @@ reserved = {
     'while': 'WHILE'
 }
 
-tokens = tokens+tuple(reserved.values())
+tokens = tokens + tuple(reserved.values())
 
 # Reglas de las expresiones regulares para tokens simples
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
 t_LSQUAREBRACKET = r'\['
@@ -78,12 +82,13 @@ t_MULTCOMMMENT = r'<\/(\s*|.*?)*\/>'
 t_LINECOMMENT = r'((\?|!).*)'
 
 # Regla que contiene caracteres ignorados (espacios y tabs)
-t_ignore  = ' \t'
+t_ignore = ' [\t\'\n]*'
 
 
-def t_ID (t):
+def t_ID(t):
     r'[A-Za-z]((_?[a-zA-Z]+)*_?([a-z0-9])+)?'
-    # El método get () devuelve un valor para la key dada. Si la key no está disponible en el diccionario, entonces devuelve el valor predeterminado 'ID'.
+    # El método get () devuelve un valor para la key dada. Si la key no está disponible en el diccionario,
+    # entonces devuelve el valor predeterminado 'ID'.
     t.type = reserved.get(t.value.lower(), 'ID')
     return t
 
@@ -93,21 +98,65 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
 def t_NUM(t):
     r'(0[0-9])|[1-9][0-9]+'
     t.value = int(t.value)
     return t
 
+
+# retorna lista de archivos de ejemplos ubicados en el directorio 'file'
+def listarArchivos(ruta=getcwd() + '\\file'):
+    return [arch.name for arch in scandir(ruta) if arch.is_file()]
+
+def lecturaArchivo(rutaArch):
+    archivo = open(rutaArch,'r')
+    lineas = archivo.read()
+    print('-----------------CÓDIGO----------------------')
+    print(lineas)
+    print('---------------------------------------------\n')
+    print('Análisis Léxico')
+    return str(lineas)
+
+
+def getcodigo():
+    #mostrar listado de archivos
+    print('-------------------------------------------')
+    print('        TALLER COMPILADORES: SCANNER')
+    print('-------------------------------------------')
+    print('Estos son los archivos de prueba disponibles en la carpeta \'file\':')
+
+    list_Arch=listarArchivos()
+    if len(list_Arch)==0:
+        print('No se encuetran archivos de prueba :(')
+    else:
+        #imprimir lista de archivos disponibles en la carpeta 'file'
+        for arch in list_Arch:
+            print(arch)
+        #seleccionar archivo
+        file_name = input('Favor de ingresar el nombre del archivo a utilizar:\n')
+        while (file_name in list_Arch)!= True:
+            print('Archivo no encontrado..\n')
+            file_name = input('Favor de ingresar el nombre del archivo a utilizar:\n')
+
+        #ruta del archivo seleccionado
+        rutaArch=getcwd() + '\\file\\'+file_name
+        #obtener código fuente del archivo
+        codigofuente=lecturaArchivo(rutaArch)
+
+    return (codigofuente)
+codigofuente = getcodigo()
+
 # Construir el lexer
 lexer = lex.lex()
 
 # Dar al analizador léxico algún input
-lexer.input('<a_=a''8')
+lexer.input(codigofuente)
 
 # Tokenize
 while True:
     tok = lexer.token()
     if not tok:
-        break      # no hay mas input
+        break  # no hay mas input
     print(tok)
 
